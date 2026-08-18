7074b3c9b780c9fed644b1087a4fe586c6832394
Muqaddas-Sharif07 <muqaddassharif762@gmail.com>
2026-08-17 21:01:07 -0700

    Update Data Agent.py

--- Data Agent.py
+++ Data Agent.py
@@ -35,25 +35,24 @@ if uploaded_file and api_key:
     user_query = st.text_input("Type your question here:")
 
   if st.button("Analyze"):
-    if user_query:
-        with st.spinner("Analyzing data..."):
-            try:
-                response = agent.run(user_query)
-                st.success("Done!")
-                st.write(response)
-            except Exception as e:
-                # Agar output Parsing Error bhi aye toh response print karwa de
-                error_msg = str(e)
-                if "Could not parse LLM output:" in error_msg:
-                    clean_res = error_msg.split("Could not parse LLM output:")[-1]
+        if user_query:
+            with st.spinner("Analyzing data..."):
+                try:
+                    response = agent.run(user_query)
                     st.success("Done!")
-                    st.write(clean_res)
-                else:
-                    st.error(f"Error: {error_msg}")
-    else:
-        st.warning("Please enter a question first!")
+                    st.write(response)
+                except Exception as e:
+                    error_msg = str(e)
+                    if "Could not parse LLM output:" in error_msg:
+                        clean_res = error_msg.split("Could not parse LLM output:")[-1]
+                        st.success("Done!")
+                        st.write(clean_res)
+                    else:
+                        st.error(f"Error: {error_msg}")
+        else:
+            st.warning("Please enter a question first!")
 
-elif not api_key:
+elif not groq_api_key:
     st.info("👈 Please enter your Groq API Key in the sidebar to get started.")
 elif not uploaded_file:
     st.info("👈 Please upload a CSV file in the sidebar.")
